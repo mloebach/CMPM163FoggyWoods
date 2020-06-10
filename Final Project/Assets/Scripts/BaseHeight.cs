33bbba9c3235aad59e5e32ec1e7447887aa0a08c
dmattgomez <demagome@ucsc.edu>
2020-06-10 11:34:04 -0700

    IT'S ALL TOGETHER!

--- Final Project/Assets/Scripts/BaseHeight.cs
+++ Final Project/Assets/Scripts/BaseHeight.cs
@@ -10,10 +10,17 @@ public class BaseHeight : MonoBehaviour
     {
        // float height = getRandHeight();
         //transform.position += new Vector3(0, height, 0);
+        
+        //make them taller! - Derek :)
+        Vector3 treeBase;
+        treeBase = transform.localScale;
+        treeBase.y += 50f;   
+        transform.localScale = treeBase;    
     }
 
     public float getRandHeight(){
         float height = Random.Range(-0.0f,2.5f);
+        
         transform.position += new Vector3(0, height, 0);
         return height;
     }
@@ -21,6 +28,7 @@ public class BaseHeight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 }
